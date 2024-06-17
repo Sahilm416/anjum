131e6a630829e7fd5d40745b7378c65cf2b50cf4
sahil <sahilmulani501@gmail.com>
2024-06-17 14:30:36 +0530

    add : initial documentaion for methods

--- apps/docs/app/docs/layout.tsx
+++ apps/docs/app/docs/layout.tsx
@@ -3,6 +3,11 @@ import { DocsLayout } from 'fumadocs-ui/layout';
 import type { ReactNode } from 'react';
 import { baseOptions } from '../layout.config';
 
+export const metadata = {
+  title: "Docs",
+  description: "project anjum docs"
+}
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout tree={pageTree} {...baseOptions}>
