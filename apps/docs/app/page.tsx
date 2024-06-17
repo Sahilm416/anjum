675882c23c0669d825ab29909975f652bfd0b304
sahil <sahilmulani501@gmail.com>
2024-06-17 23:46:50 +0530

    add : homepage name

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">Hello Anjum</h1>
       <p className="text-muted-foreground">
         You can open{' '}
         <Link href="/docs" className="text-foreground font-semibold underline">
