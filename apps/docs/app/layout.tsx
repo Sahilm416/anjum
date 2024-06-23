4233115e73574d4ae54bc683b5d13df21aa67825
sahil <sahilmulani501@gmail.com>
2024-06-23 23:39:30 +0530

    add : metadata on home page

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,11 +1,16 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
+
+export const metadata = {
+  title: "Project anjum",
+  description: "simple email-pass auth for next.js",
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
