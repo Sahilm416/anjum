562cfcb4474485c56c259870394366debee1ad39
sahil <sahilmulani501@gmail.com>
2024-06-23 23:33:02 +0530

    add : project contributors

--- apps/docs/components/contributors.tsx
+++ apps/docs/components/contributors.tsx
@@ -1,26 +1,38 @@
-import Image from "next/image";
-const cont = [
-  { name: "sahil", profile: "https://github.com/sahilm416.png" },
-  { name: "sahil", profile: "https://github.com/github.png" },
-  { name: "sahil", profile: "https://github.com/nextjs.png" },
-  { name: "sahil", profile: "https://github.com/shadcn.png" },
-  { name: "sahil", profile: "https://github.com/rauchg.png" },
-];
-const Contributors = () => {
+import { Octokit } from "@octokit/rest";
+import Link from "next/link";
+
+const Contributors = async () => {
+  const octokit = new Octokit();
+
+  const res = await octokit.rest.repos.listContributors({
+    owner: "sahilm416",
+    repo: "anjum",
+  });
+
+  if (!res || !res.data || res.data.length < 1) {
+    return (
+      <div className="text-2xl text-red-500 p-5">
+        <h2>Failed to load contributors!</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full flex flex-col gap-5 items-center">
       <h1 className="text-4xl font-semibold">Contributors</h1>
       <div className="flex gap-2">
-        {cont.map((c, i) => {
+        {res.data.map((c, i) => {
           return (
-            <div key={i}>
-              <Image
-                src={c.profile}
-                height={50}
-                width={50}
-                alt="profile"
-                className="rounded-full cursor-pointer border"
-              />
+            <div className="border rounded-full" key={i}>
+              <Link target="blanc" href={c.html_url!}>
+                <img
+                  src={c.avatar_url}
+                  width={50}
+                  height={50}
+                  className="rounded-full"
+                  title={c.login}
+                />
+              </Link>
             </div>
           );
         })}
